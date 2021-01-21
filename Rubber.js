bd1fa3a3980676657cd5c22d265ab1bf29e7a348
Krishna Agrawal <76589418+KRishnaAgRaWalL@users.noreply.github.com>
2021-01-21 17:28:36 +0530

    Add files via upload

---
+++ Rubber.js
@@ -0,0 +1,38 @@
+class Rubber {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          'friction':0.3,
+          'density':2.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+  
+    display(){
+      var pos =this.body.position;
+     var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill("grey")
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  }
+  
+  /*
+  1. var
+  2. define options
+  3. var = Bodies. something..
+  4. Add it to the world
+  
+  5. rectMode()
+  6. rect()
+  */
+  
